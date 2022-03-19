05566a8f417410e514d8fc2860614be068f64560
mrfrosk <heroker44@gmail.com>
2022-03-19 16:20:46 +0300

    Добавлено поле "password"

--- codewars/models.py
+++ codewars/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=30)
     icon = models.CharField(max_length=500)
+    password = models.CharField(max_length=30, null=False)
 
 
 class Post(models.Model):
