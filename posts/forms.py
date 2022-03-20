4a443b9634bf240ddc622d4fee42269fd701555e
mrfrosk <heroker44@gmail.com>
2022-03-20 16:17:38 +0300

    Измена форма регистрации

--- posts/forms.py
+++ posts/forms.py
@@ -1,6 +1,16 @@
 from django import forms
+from django.contrib.auth.hashers import make_password
+
+from .models import User
 
 
-class RegistrationForm(forms.Form):
-    username = forms.CharField(label='Имя пользователя', max_length=30)
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput())
+class RegistrationForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('email', 'name', 'password')
+
+    # email = forms.EmailField(label='Email')
+    # username = forms.CharField(label='Имя пользователя', max_length=50)
+    # password = forms.CharField(label='Пароль', widget=forms.PasswordInput(), max_length=50)
+
+
