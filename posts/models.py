d613bd9ee6f96a4c3acf7867c0e339315764f022
mrfrosk <heroker44@gmail.com>
2022-03-20 16:18:56 +0300

    Изменена модель "User" и модель "Post"

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,19 @@
+from django.contrib.auth.hashers import make_password
 from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=30)
-    password = models.CharField(max_length=30, default='1234')
+    name = models.CharField(max_length=50, null=False)
+    email = models.EmailField(null=False, unique=True)
+    password = models.CharField(max_length=50, null=False)
+
+    def set_password(self, password):
+        self.password = make_password(password)
 
 
 class Post(models.Model):
     name = models.CharField(max_length=30)
+    complexity = models.CharField(max_length=3, null=False)
+    language = models.CharField(max_length=30, null=False)
     link = models.CharField(max_length=300)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
