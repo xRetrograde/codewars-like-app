00d4e5584543014578092e5a671e418faa299348
mrfrosk <heroker44@gmail.com>
2022-03-20 18:27:27 +0300

    Добавлена форма "LoginForm"

--- posts/forms.py
+++ posts/forms.py
@@ -7,10 +7,14 @@ from .models import User
 class RegistrationForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('email', 'name', 'password')
+        fields = ('name', 'email')
 
     # email = forms.EmailField(label='Email')
     # username = forms.CharField(label='Имя пользователя', max_length=50)
-    # password = forms.CharField(label='Пароль', widget=forms.PasswordInput(), max_length=50)
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(), max_length=50)
 
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(), max_length=50)
 
