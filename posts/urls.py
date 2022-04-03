76fb81fd7e388faf0ff6f4abd0be5391d434e14b
human <sanina228user@gmail.com>
2022-04-03 22:18:51 +0300

    Удален бесполезный роутинг "new-post"

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('profile', views.profile),
-    path('new-post', views.new_post),
     path('registration', views.registration),
     path('login', views.login),
     path('add-post', views.add_post),
