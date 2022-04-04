4ce692e01ec81ae962e39333c47f9372d8651d17
mrfrosk <heroker44@gmail.com>
2022-04-04 12:41:32 +0300

    Добавленна защита от подделки куки

--- posts/utils.py
+++ posts/utils.py
@@ -1,9 +1,12 @@
 from django.shortcuts import redirect
+from django.contrib.auth.hashers import make_password
+from .models import User
 
 
 def auth_required(view):
     def wrapped(request):
-        if 'name' and 'email' in request.session:
+        user = User.objects.filter(email=request.session.get('email')).exists()
+        if user:
             return view(request)
 
         return redirect('/login')
@@ -20,3 +23,8 @@ def unauth_required(view):
 
     return wrapped
 
+
+def set_session_info(request, name, email, password):
+    request.session['name'] = name
+    request.session['email'] = email
+    request.session['password'] = make_password(password)
